5a5dabe9c77571d0c6d820fd3a585a08f30b7ff9
Lukhverchyk Vitali <luhverchikv@gmail.com>
2023-06-07 15:15:07 +0300

    Second commit with logger update gitignore no autodelit working version

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
 from aiogram.types import Message, CallbackQuery, ReplyKeyboardMarkup, ParseMode
 from aiogram.utils import executor
 from aiogram.dispatcher.filters import Text
-from aiogram_calendar import simple_cal_callback, SimpleCalendar # pip install aiogram-calendar
+from aiogram.utils.exceptions import MessageCantBeDeleted, MessageToDeleteNotFound
+from aiogram_calendar import simple_cal_callback, SimpleCalendar  # pip install aiogram-calendar
+from contextlib import suppress
 import datetime
 from config import API_TOKEN
 from loguru import logger
+import asyncio
 
 # API_TOKEN = '' uncomment and insert your telegram bot API key here
 
@@ -18,8 +21,14 @@ logger.add('ihfo.log', format="{time} {level} {message}", level="INFO",
            rotation="1 week", compression="zip")
 
 # star keybord ('Сегодня', 'Выбрать Дату', 'Помощь')
-start_kb = ReplyKeyboardMarkup(resize_keyboard=True,)
+start_kb = ReplyKeyboardMarkup(resize_keyboard=True, )
 start_kb.row('Сегодня', 'Выбрать Дату', 'Помощь')
+
+
+async def delete_message(message: types.Message, seconds: int = 0):
+    await asyncio.sleep(seconds)
+    with suppress(MessageCantBeDeleted, MessageToDeleteNotFound):
+        await message.delete()
 
 
 @dp.message_handler(commands=['start'])
@@ -32,6 +41,8 @@ async def cmd_start(message: Message):
 @dp.message_handler(Text(equals=['Выбрать Дату'], ignore_case=True))
 async def nav_cal_handler(message: Message):
     await message.answer("Выберите дату: ", reply_markup=await SimpleCalendar().start_calendar())
+    asyncio.create_task(delete_message(message, 5))
+    # asyncio.create_task(delete_message(answer, 5))
 
 
 # simple calendar usage
@@ -41,27 +52,30 @@ async def process_simple_calendar(callback_query: CallbackQuery, callback_data: 
     future = date + datetime.timedelta(days=125)
     if selected:
         logger.info(f'Пользователь {callback_query.message.from_user.id} нажал "Календарь"')
-        await callback_query.message.answer(
-            f'Вы выбрали: {date.strftime("%d.%m.%Y")}\nЧерез 126 дней: {future.strftime("%d.%m.%Y")}',
-            reply_markup=start_kb
-        )
+        answer = await callback_query.message.answer(f'Вы выбрали: {date.strftime("%d.%m.%Y")}\n'
+                                                     f'Через 126 дней: {future.strftime("%d.%m.%Y")}')
+        asyncio.create_task(delete_message(callback_query.message, 5))
+        asyncio.create_task(delete_message(answer, 10))
 
 
 @dp.message_handler(Text(equals=['Сегодня'], ignore_case=True))
 async def simple_cal_handler(message: Message):
     logger.info(f'Пользователь {message.from_user.id} нажал "Сегодня"')
     now = datetime.datetime.now()
-    future = now + datetime.timedelta(days=125) # на один день меньше, так как счет не с 0, а с 1.
+    future = now + datetime.timedelta(days=125)  # на один день меньше, так как счет не с 0, а с 1.
     future_date = future.strftime('%d.%m.%Y')
-    await message.answer(f"Сегодня: {now.strftime('%d.%m.%Y')}\nЧерез 126 дней: {future_date}")
+    answer = await message.answer(f"Сегодня: {now.strftime('%d.%m.%Y')}\nЧерез 126 дней: {future_date}")
+    asyncio.create_task(delete_message(message, 5))
+    asyncio.create_task(delete_message(answer, 10))
 
 
 @dp.message_handler(Text(equals=['Помощь'], ignore_case=True))
 async def simple_cal_handler(message: Message):
     logger.info(f'Пользователь {message.from_user.id} нажал "Помощь"')
-    await message.answer(f'Лучше звоните <a href="https://t.me/ImAntonbatura">Антону</a>',
-                             parse_mode=ParseMode.HTML)
-
+    answer = await message.answer(f'Мой <a href="https://t.me/luhverchik">номер</a>',
+                                  parse_mode=ParseMode.HTML)
+    asyncio.create_task(delete_message(message, 5))
+    asyncio.create_task(delete_message(answer, 10))
 
 
 if __name__ == '__main__':
